8eb17d56c59b28a1276bc1243ad817632d5cf668
dreacare-kogure <sumire.kogure@g.dreamcareer.co.jp>
2025-01-16 15:07:49 +0900

    20250116_共有時

--- js/function.js
+++ js/function.js
@@ -1,40 +1,47 @@
-const tileTitleList = ["サステナビリティ", "事業領域", "MISSION"];
+const tileTitleList = ["事業領域", "サステナビリティ", "MISSION"];
 const tileTextList = [
-  "PET樹脂生産のLCA<br>・衣料品のリサイクル<br>・ペットボトルの<br>　リサイクル<br><span class='mainTile__annotation'>※LCA<br>(ライフサイクルアセスメント)</span>",
   "・商用プラント事業<br>・技術ライセンス事業<br>・アパレル事業<br>・ボトル事業",
-  "あらゆるものを<br>循環させる<br>We circulate<br> our world"
-]
+  "PET樹脂生産のLCA<br>・衣料品のリサイクル<br>・ペットボトルの<br>　リサイクル<br><span class='mainTile__annotation'>※LCA<br>(ライフサイクルアセスメント)</span>",
+  "あらゆるものを<br>循環させる<br>We circulate<br> our world",
+];
+const tileImageList = [
+  "assets/img/mv_tile-icon_graph.png",
+  "assets/img/mv_tile-icon_recycle.png",
+  "assets/img/mv_tile-icon_mission.png",
+];
+const tileMiniImageList = [
+  "assets/img/mv_mini-tile-icon_graph.png",
+  "assets/img/mv_mini-tile-icon_recycle.png",
+  "assets/img/mv_mini-tile-icon_mission.png",
+];
 
 let currentIndex = 0; // 現在のメッセージのインデックスを管理
 let intervalId; // setIntervalのIDを保存する変数
 
-// changeText関数の呼び出し
+// changeTile関数の呼び出し
 function startInterval() {
-  intervalId = window.setInterval(changeText, 5000); // 5秒ごとに変更（フェードアウト時間を考慮）
+  intervalId = window.setInterval(changeTile, 5000); // 5秒ごとに変更（フェードアウト時間を考慮）
 }
 
 function stopInterval() {
   clearInterval(intervalId); // setIntervalを停止
 }
 
-function changeText() {
-  // mainTile__title と mainTile__text は1つずつなので、querySelectorを使用
+function changeTile() {
+  const mainTileWrap = document.querySelector(".tileWrap");
   const mainTile__title = document.querySelector(".mainTile__title");
   const mainTile__text = document.querySelector(".mainTile__text");
+  const mainTile__image = document.querySelector(".mainTile__image");
 
-  // mainTile__title と mainTile__text は1つなので、ここで直接操作
-  mainTile__title.classList.add("fade-out");
-  mainTile__text.classList.add("fade-out");
-
-  // subTile__title は複数存在するので、getElementsByClassName を使用
+  const subTileWraps = document.getElementsByClassName("subTileWrap");
   const subTile__titles = document.getElementsByClassName("subTile__title");
+  const subTile__images = document.getElementsByClassName("subTile__image");
 
-  // すべての subTile__title 要素を一旦フェードアウトさせる
-  for (let i = 0; i < subTile__titles.length; i++) {
-    const subTile__title = subTile__titles[i];
-
-    // subTile__title のみにフェードアウトのクラスを追加
-    subTile__title.classList.add("fade-out");
+  // mainTileとsubTileの中身を消す
+  mainTileWrap.classList.add("transparent");
+  for (let i = 0; i < subTileWraps.length; i++) {
+    const subTileWrap = subTileWraps[i];
+    subTileWrap.classList.add("transparent");
   }
 
   // 少し待ってからテキストを更新してフェードイン
@@ -42,20 +49,27 @@ function changeText() {
     // mvTitles配列からtitleとtextを取得して設定
     const currentTitle = tileTitleList[currentIndex % tileTitleList.length];
     const currentText = tileTextList[currentIndex % tileTextList.length];
+    const currentImage = tileImageList[currentIndex % tileImageList.length];
 
     mainTile__title.innerText = "ー" + currentTitle + "ー";
     mainTile__text.innerHTML = currentText;
+    mainTile__image.src = currentImage;
 
-    // フェードインのクラスを削除
-    mainTile__title.classList.remove("fade-out");
-    mainTile__text.classList.remove("fade-out");
-
-    // 各要素のテキストを更新
-    for (let i = 0; i < subTile__titles.length; i++) {
+    // サブタイルのテキストと画像を更新
+    for (let i = 0; i < subTileWraps.length; i++) {
       const currentSubTitle = tileTitleList[(currentIndex + i + 1) % tileTitleList.length];
       const subTile__title = subTile__titles[i];
+      const currentSubImage = tileMiniImageList[(currentIndex + i + 1) % tileMiniImageList.length];
+      const subTile__image = subTile__images[i];
       subTile__title.innerText = currentSubTitle;
-      subTile__title.classList.remove("fade-out");
+      subTile__image.src = currentSubImage;
+    }
+
+    // mainTileとsubTileの中身を表示
+    mainTileWrap.classList.remove("transparent");
+    for (let i = 0; i < subTileWraps.length; i++) {
+      const subTileWrap = subTileWraps[i];
+      subTileWrap.classList.remove("transparent");
     }
 
     // インデックスを進める
@@ -65,7 +79,7 @@ function changeText() {
     if (currentIndex >= tileTitleList.length) {
       currentIndex = 0;
     }
-  }, 1500); // 1.5秒後にテキストを更新（フェードアウト後の遅延）
+  }, 2000); // 1.5秒後にテキストを更新（フェードアウト後の遅延）
 }
 
 // .mainTile 要素に対してホバーイベントを設定
@@ -83,4 +97,3 @@ mainTileElement.addEventListener("mouseleave", () => {
 
 // ページがロードされた時にインターバルを開始
 startInterval();
-
